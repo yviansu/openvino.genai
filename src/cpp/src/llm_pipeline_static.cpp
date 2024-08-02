da325d8dd78de875332f0be358ad825e4fef7f3a
Su Yihan <yihan.su@intel.com>
2024-08-02 14:30:17 +0800

    use fill_random to fill input tensor
    Signed-off-by: Su Yihan <yihan.su@intel.com>

--- src/cpp/src/llm_pipeline_static.cpp
+++ src/cpp/src/llm_pipeline_static.cpp
@@ -90,6 +90,29 @@ void reshape_to_static(std::shared_ptr<ov::Model> model,
     model->reshape(new_shapes);
 }
 
+template <typename T>
+using uniformDistribution = typename std::conditional<
+    std::is_floating_point<T>::value,
+    std::uniform_real_distribution<T>,
+    typename std::conditional<std::is_integral<T>::value, std::uniform_int_distribution<T>, void>::type>::type;
+
+template <typename T, typename T2>
+static inline void fill_random(ov::Tensor& tensor,
+                               T rand_min = std::numeric_limits<uint8_t>::min(),
+                               T rand_max = std::numeric_limits<uint8_t>::max()) {
+    std::mt19937 gen(0);
+    size_t tensor_size = tensor.get_size();
+    if (0 == tensor_size) {
+        throw std::runtime_error(
+            "Models with dynamic shapes aren't supported. Input tensors must have specific shapes before inference");
+    }
+    ov::float16* data = tensor.data<ov::float16>();
+    uniformDistribution<T2> distribution(rand_min, rand_max);
+    for (size_t i = 0; i < tensor_size; i++) {
+        data[i] = static_cast<ov::float16>(distribution(gen));
+    }
+}
+
 void fill_tensor(ov::Tensor tensor, int64_t fill_val, size_t offset = 0u) {
     ov::float16* tensor_data = tensor.data<ov::float16>();
     std::fill(tensor_data + offset, tensor_data + tensor.get_size(), (ov::float16)fill_val);
@@ -142,19 +165,6 @@ StaticLLMPipeline::StaticLLMPipeline(
     const ov::AnyMap& config
 ) : LLMPipelineImplBase(tokenizer,
                         utils::from_config_json_if_exists(path)) {
-    /* NB: Static LLM pipeline consists of two models,
-       first to process the input prompt (prefill), second to use in generation loop (kvcache)
-
-       Initialization assumes multiple steps:
-       1) Read the template model - this will be kvcache model
-       2) Expose KV-cache input and output layers from kvcache model
-       3) Clone the model - this will be prefill
-       3) Reshape both models to static shape
-       4) Add slices to KV-cache inputs for kvcache model, this will make input and output KV-cache
-          layers to have the same shape and allow outputs writes directly to inputs for the next iteration.
-       5) Compile both models
-       6) Initialize input tensors for kvcache and prefill models
-    */
     ov::Core core;
     // (1) Read the template model - this will be kvcache model
     std::ifstream modelStream("C:\\WorkSpace\\Yihan\\models\\Phi-3\\ovmergev6\\vpuip.blob", std::ios_base::binary | std::ios_base::in);
@@ -166,7 +176,6 @@ StaticLLMPipeline::StaticLLMPipeline(
     m_kvcache_request = core.import_model(modelStream, device, latency).create_infer_request();
     std::cout << "kvcache blob file imported on " << device << std::endl;
     modelStream.close();
-
     // (4) Reshape both models to static shape
     m_kvcache_desc = KVCacheDesc { 1024u, 0u };
     const uint32_t max_prompt_size = m_kvcache_desc.total_size;
@@ -241,6 +250,12 @@ inline double get_duration_ms(Time::time_point& startTime, Time::time_point& end
     return std::chrono::duration_cast<ns>(endTime - startTime).count() * 0.000001;
 };
 
+static inline std::string double_to_string(const double number) {
+    std::stringstream ss;
+    ss << std::fixed << std::setprecision(2) << number;
+    return ss.str();
+}
+
 EncodedResults StaticLLMPipeline::generate(
     const EncodedInputs& inputs,
     OptionalGenerationConfig generation_config,
@@ -249,123 +264,42 @@ EncodedResults StaticLLMPipeline::generate(
     const auto& compiledModel = m_kvcache_request.get_compiled_model();
     ov::element::Type layer_type = ov::element::f16;
     
-    for (int j = 0; j < compiledModel.inputs().size() - 1; ++j) {
-        std::string input_name = compiledModel.inputs()[j].get_any_name();
-        ov::Tensor zero_tensor = ov::Tensor(layer_type, ov::Shape(compiledModel.inputs()[j].get_shape()));
-        m_kvcache_request.set_tensor(input_name, zero_tensor);
+    // for (int j = 0; j < compiledModel.inputs().size() - 1; ++j) {
+    //     std::string input_name = compiledModel.inputs()[j].get_any_name();
+    //     ov::Tensor zero_tensor = ov::Tensor(layer_type, ov::Shape(compiledModel.inputs()[j].get_shape()));
+    //     m_kvcache_request.set_tensor(input_name, zero_tensor);
+    // }
+    for (const ov::Output<const ov::Node>& model_input : compiledModel.inputs()) {
+        fill_random<short, short>(m_kvcache_request.get_tensor(model_input));
     }
 
-    ov::Tensor input_ids;
-    ov::Tensor attention_mask;
-
-    if (auto data = std::get_if<ov::Tensor>(&inputs)) {
-        input_ids = *data;
-        attention_mask = ov::genai::utils::init_attention_mask(input_ids);
-    } else if (auto data = std::get_if<TokenizedInputs>(&inputs)) {
-        input_ids = data->input_ids;
-        attention_mask = data->attention_mask;
+    auto startTime = Time::now();
+ 
+    m_kvcache_request.infer();
+    auto duration_ms = get_duration_ms_till_now(startTime);
+    std::cout << "First inference took " << double_to_string(duration_ms) << " ms" << std::endl;
+    
+    size_t iteration = 0;
+    startTime = Time::now();
+    while ( iteration < 100) {
+        // for (int j = 0; j < compiledModel.inputs().size() - 1; ++j) {
+        //     std::string input_name = compiledModel.inputs()[j].get_any_name();
+        //     ov::Tensor zero_tensor = ov::Tensor(layer_type, ov::Shape(compiledModel.inputs()[j].get_shape()));
+        //     m_kvcache_request.set_tensor(input_name, zero_tensor);
+        // }
+        m_kvcache_request.infer();
+        ++iteration;
     }
-
-    if (input_ids.get_shape().at(0) > 1u) {
-        OPENVINO_THROW("Currently only batch size=1 is supported");
-    }
-
-    GenerationConfig config = (generation_config.has_value()) ? *generation_config : m_generation_config;
-    // If eos_token_id was not provided, take value from default m_generation_config
-    if (config.eos_token_id == -1)
-        config.set_eos_token_id(m_generation_config.eos_token_id);
-    config.validate();
-
-    std::shared_ptr<StreamerBase> streamer_ptr;
-    if (auto streamer_obj = std::get_if<std::monostate>(&streamer)) {
-        streamer_ptr = nullptr;
-    } else if (auto streamer_obj = std::get_if<std::shared_ptr<StreamerBase>>(&streamer)) {
-        streamer_ptr = *streamer_obj;
-    } else if (auto callback = std::get_if<std::function<bool(std::string)>>(&streamer)) {
-        streamer_ptr = std::make_shared<TextCallbackStreamer>(m_tokenizer, *callback);
-    }
-
-    if (!config.is_greedy_decoding()) {
-        OPENVINO_THROW("Currently only greedy decoding is supported");
-    }
+    auto totalDuration = get_duration_ms_till_now(startTime);
+    double fps = 1000.0 * iteration / totalDuration;
+    std::cout << "Count:               " << iteration << " iterations" << std::endl;
+    std::cout << "Duration:            " << double_to_string(totalDuration) << " ms" << std::endl;
+    std::cout << "Throughput:          " << double_to_string(fps) << " FPS" << std::endl;
 
     ov::genai::EncodedResults results;
-    // NB: Only batch=1 is supported now
     results.scores.resize(1u);
     results.scores[0] = 0u;
     results.tokens.resize(1u);
-
-    // NB: Check if input prompt less than maximum size
-    auto prompt_len = input_ids.get_size();
-    if (prompt_len > m_kvcache_desc.total_size) {
-        OPENVINO_THROW("Currently static pipeline only process up to " + std::to_string(m_kvcache_desc.total_size) + " tokens");
-    }
-
-    auto start_time = Time::now();
-
-    const auto kStartInputKVCacheLayers = 0u;
-    const auto kStartOutputKVCacheLayers = 1u;
-
-    for (int i = 0; i < prompt_len; ++i) {
-        m_kvcache_request.infer();
-    }
-
-    // Generation stage
-    ov::float16 last_token = 0;
-    results.tokens[0].push_back(last_token);
-    if (streamer_ptr && streamer_ptr->put(last_token)) {
-        return results;
-    }
-
-    auto first_token_time = Time::now();
-    const size_t max_tokens = config.get_max_new_tokens(prompt_len);
-
-    for (int i = prompt_len; i < max_tokens; ++i) {
-        m_kvcache_request.infer();
-  
-        last_token = 0;
-        results.tokens[0].push_back((int64_t)last_token);
-
-        if (streamer_ptr && streamer_ptr->put(last_token)) {
-            break;
-        }
-
-        if ((int64_t)last_token == m_generation_config.eos_token_id) {
-            break;
-        }
-
-    }
-
-    int token_count = results.tokens[0].size();
-
-    auto end_time = Time::now();
-    std::cout << "Performance metrics: " << std::endl;
-    std::cout << "Generated tokens: " << token_count << std::endl;
-    std::cout << "First token generation time: " << get_duration_ms(start_time, first_token_time) << std::endl;
-    std::cout << "Generation average latency: " << get_duration_ms(first_token_time, end_time)/(token_count - 1) << std::endl;
-    std::cout << "token/s: " << (token_count - 1)/(get_duration_ms(first_token_time, end_time)/1000) << std::endl;
-    std::cout << "Generation time: " << get_duration_ms(start_time, end_time) << std::endl;
-
-    std::cout << "Tokens:\n";
-    for (const auto& token_vector : results.tokens) {
-        std::cout << "[";
-        for (size_t i = 0; i < token_vector.size(); ++i) {
-            std::cout << token_vector[i];
-            if (i != token_vector.size() - 1) {
-                std::cout << ", ";
-            }
-        }
-        std::cout << "]\n";
-    }
-
-    std::cout << "Scores:\n[";
-    for (size_t i = 0; i < results.scores.size(); ++i) {
-        std::cout << results.scores[i];
-        if (i != results.scores.size() - 1) {
-            std::cout << ", ";
-        }
-    }
-    std::cout << "]\n";
     return results;
 }
 
